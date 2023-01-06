0753a768c96666fd6ccf6864affabaf688341128
IIHEXYII <vetletessem@gmail.com>
2023-01-06 11:21:22 +0100

    finished lesson 3

--- jeeves/index.js
+++
@@ -1,6 +0,0 @@
-const logEvents = require("./logEvents");
-const EventEmitter = require("events");
-
-class TheEmitter extends EventEmitter {}
-const emitter = new TheEmitter();
-emitter.on("log", (msg) => logEvents(msg));
