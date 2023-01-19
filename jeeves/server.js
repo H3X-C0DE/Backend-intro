ba2794a10b01b1073b027d9cc7b080f47a5ac121
IIHEXYII <vetletessem@gmail.com>
2023-01-19 11:09:20 +0100

    added verify, cookies, refresh & logout

--- jeeves/server.js
+++ jeeves/server.js
@@ -7,6 +7,8 @@ const app = express();
 const errorHandler = require("./middleware/errorHandler");
 const corsOptions = require("../jeeves/config/corsOptions");
 const { text } = require("express");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
 
 // Port definition
 const PORT = process.env.PORT || 3500;
@@ -14,19 +16,26 @@ const PORT = process.env.PORT || 3500;
 // Middleware
 app.use(logger);
 app.use(cors());
+
 //corsOptions
 app.use(cors(corsOptions));
 
 app.use(errorHandler);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
-app.use("/employees", require("./routes/api/employees"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+// Login To Access //
+app.use(verifyJWT);
+app.use("/employees", require("./routes/api/employees"));
 
 app.get("/*", (req, res) => {
   res.status(404);
