73112e17e5490f1b84411757199664d42906d298
IIHEXYII <vetletessem@gmail.com>
2023-01-19 11:10:21 +0100

    authController code update

--- jeeves/controllers/authController.js
+++ jeeves/controllers/authController.js
@@ -1,29 +1,58 @@
-const usersDB =
-{
-    users: require("../model/users.json"),
-    setUsers: function (data) {this.users = data}
-}
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
 
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const fsPromises = require("fs").promises;
+const path = require("path");
 
-const handleLogin = async (req, res) =>
-{
-    const {user, pwd} = req.body;
-    if (!user || !pwd) return res.status(400).json({"message": "Username and password required"});
+require("dotenv").config();
 
-    const foundUser = usersDB.users.find(person => person.username === user);
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res.status(400).json({ message: "Username and password required" });
+  console.log(usersDB.users);
+  //   console.log(user);
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  //   console.log(foundUser);
+  //   console.log(user);
 
-    if(!foundUser) return res.sendStatus(401);
+  if (!foundUser) return res.sendStatus(401);
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    const accessToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" }
+    );
 
-    const match = await bcrypt.compare(pwd, foundUser.password);
-    if (match)
-    {
-        res.json({"success": `User ${user} logged in`})
-    }
-    else
-    {
-        res.sendStatus(401);
-    }
-}
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: "1d" }
+    );
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, "..", "model", "users.json"),
+      JSON.stringify(usersDB.users)
+    );
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
 
-module.exports = {handleLogin};
+module.exports = { handleLogin };
